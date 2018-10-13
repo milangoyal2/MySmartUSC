5948dda360b4e4c9779aeb456c16304ea5fa94d8
Aditya Shah <shahaa@usc.edu>
2018-10-13 16:58:54 -0400

    Test push. Added loginActivity.

--- app/src/main/java/com/example/bruins/mysmartusc/MainActivity.java
+++ app/src/main/java/com/example/bruins/mysmartusc/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.bruins.mysmartusc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /*
+    if not logged in, take to LogInActivity
+    (loginActivity sets a user token)
+    */
+    if (Util.getToken() != null) {
+        Intent loginIntent = Intent(this, LoginActivity::; class.java);
+        startActivity(loginIntent);
+    }
+
+
+
+    finish();
 }
