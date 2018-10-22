631a700a6a906f9b177dd9ffd64ea1d8ceb1cb7f
Milan Goyal <Milan@Milans-MacBook-Pro.local>
2018-10-21 19:49:56 -0700

    updating Globals

--- app/src/main/java/com/example/bruins/mysmartusc/Globals.java
+++ app/src/main/java/com/example/bruins/mysmartusc/Globals.java
@@ -1,24 +1,26 @@
 package com.example.bruins.mysmartusc;
 
+import com.sun.mail.imap.IMAPStore;
+
 import javax.mail.Session;
 
 public class Globals {
     private static Globals instance;
 
     // Global variable
-    private Session session;
+    private IMAPStore store;
     private ServiceClass service;
     // Restrict the constructor from being instantiated
     private Globals(){
-        session = null;
+        store = null;
         service = null;
     }
 
-    public void setSession(Session s){
-        this.session = s;
+    public void setStore(IMAPStore s){
+        this.store = s;
     }
-    public Session getSession(){
-        return session;
+    public IMAPStore getStore(){
+        return store;
     }
     public void setService(ServiceClass s) {
         this.service = s;
